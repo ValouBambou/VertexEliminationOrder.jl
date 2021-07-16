1ea06b21ad0447aeae7afd178ce15da97a566e9b
Valentin Trophime <theval358@gmail.com>
2021-07-16 16:49:39 +0200

    better dissection

--- src/flowcutter.jl
+++ src/flowcutter.jl
@@ -5,7 +5,7 @@
 end
 
 
-function split_cell(c::Cell)::Cell
+function split_cell(c::Cell)
     # get a separator to split the cell using flow cutter algorithm
     nothing
 end
@@ -35,22 +35,33 @@ function nested_dissection(g::SimpleGraph{Int64})
     # init cells sets
     open_cells = PriorityQueue{Cell, Int64}(Base.Order.Reverse)
     enqueue!(open_cells, Cell(boundary = Set([]), interior = vertices(g)), nv(g))
-    finals_cells::Set{Cell} = Set()
+    finals_cells::Vector{Cell} = []
     max_bag_size_finals = 0
 
     c = peek(open_cells).first
     while c.bag_size > max_bag_size_finals
         c = dequeue!(open_cells)
-        # split the current cell (using a separator from flow cutter) gives a set of cells
-        cf = split_cell(c)
-
-        # need to catch the final cell cf to add in the final cells set
+        # split the current cell (using a separator from flow cutter)
+        cf, co = split_cell(c)
 
         # updating finals cells set and its max bag size
-        push!(open_cells, cf)
+        push!(finals_cells, cf)
         max_bag_size_finals = max(max_bag_size_finals, cf.bag_size)
 
+        # update the open cells set
+        for c in co
+            enqueue!(open_cells, c, c.bag_size)
+        end
         # the next cell is the max_bag_size
         c = peek(open_cells).first
     end
+    treewidth = max(map(v -> v.bag_size, finals_cells), map(v -> v.bag_size, open_cells))
+    order = vcat(
+        map(
+            cell -> vcat(cell.interior, cell.boundary),
+            vcat(collect(keys(open_cells)), reverse(finals_cells))
+            )...        
+    )
+
+    return (,
 end
