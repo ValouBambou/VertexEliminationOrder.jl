27c11ef20a363ed912d8748dc119ae8131cdff5e
Valentin Trophime <theval358@gmail.com>
2021-07-20 14:22:20 +0200

    custom BFS for flowcutter

--- src/VertexEliminationOrder.jl
+++ src/VertexEliminationOrder.jl
@@ -3,6 +3,7 @@ module VertexEliminationOrder
 using LightGraphs
 using Parameters
 using DataStructures
+using StatsBase
 
 include("heuristics.jl")
 export minfill!, minwidth!
--- src/flowcutter.jl
+++ src/flowcutter.jl
@@ -1,24 +1,79 @@
 @with_kw struct Cell
-    boundary::Set{Int64}
-    interior::Set{Int64}
+    boundary::Vector{Int64}
+    interior::Vector{Int64}
     bag_size::Int64 = length(boundary) + length(interior)
 end
 
+function bfs_non_satured_path(
+        g::SimpleGraph,
+        root::Int64,
+        objective::Set{Int64},
+        capacity_matrix::Array{Int64, 2},
+        flow_matrix::Array{Int64, 2})
+
+    visited = fill(false, nv(g))
+    queue = Queue{Int64}()
+    visited[root] = true
+    enqueue!(queue, root)
+    while !isempty(queue)
+        v = dequeue!(queue)
+        if v in objective
+            return v
+        end
+        for n in neighbors(g, v)
+            if (flow_matrix[v, n] != capacity_matrix[v, n]) && (!visited[n])
+                visited[v] = true
+                enqueue!(queue, v)
+            end
+        end
+    end
+end
+
+function forward_grow!(
+        set::Set{Int64},
+        g::SimpleGraph,
+        capacity_matrix::Array{Int64, 2},
+        flow_matrix::Array{Int64, 2})
+
+    to_explore = setdiff(vertices(g), set)
+    for node in to_explore
+        for x in set
+            queue = Queue{Int64}()
+        end
+    end
+end
+
+function flowcutter(g::SimpleGraph, s::Int64, t::Int64)
+    S, T = [s], [t]
+    reachable_S, reachable_T = [s], [t]
+
+end
+
 """
-    split_cell(c)
+    split_cell(cell, g)
 Computes a separator using flow cutter algorithm and return one final cell and
 multiple open cells for nested recursion.
 
 # Arguments
-- `c::Cell` the cell (part of a graph) to split
+- `cell::Cell` the cell to split
+- `g::SimpleGraph` the graph which contains the cell
 
 # Return
 - `cf::Cell` the final cell from separator
 - `co::Vector{Cell}` the rest of open cells from the separation
 """
-function split_cell(c::Cell)
-    # get a separator to split the cell using flow cutter algorithm
-    nothing
+function split_cell(cell::Cell, g::SimpleGraph)
+    # be careful with subgraph they are new graph with their own indices
+    # use vmap to map the i index of the subgraph to vmap[i] its original index
+    gc, vmap = induced_subgraph(g, cell.interior)
+    # choose randomly 2 distinct nodes to became source and target for flowcutter
+    s, t = sample(cell.interior, 2, replace = false)
+    sep = flowcutter(gc, s, t)
+    # TODO fix cells co for return values, and try to implement this without Cell
+    return (
+        Cell(boundary = cell.boundary, interior = sep),
+        setdiff(vertices(gc), sep)
+    )
 end
 
 """
@@ -45,7 +100,7 @@ julia> nested_dissection(G)
 function nested_dissection(g::SimpleGraph{Int64})
     # init cells sets
     open_cells = PriorityQueue{Cell, Int64}(Base.Order.Reverse)
-    enqueue!(open_cells, Cell(boundary = Set([]), interior = vertices(g)), nv(g))
+    enqueue!(open_cells, Cell(boundary = [], interior = vertices(g)), nv(g))
     finals_cells::Vector{Cell} = []
     max_bag_size_finals = 0
 
