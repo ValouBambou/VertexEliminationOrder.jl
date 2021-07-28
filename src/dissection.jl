3042bdbce9af29c28379fa27f417d08546ea6bf3
Valentin Trophime <theval358@gmail.com>
2021-07-28 15:26:51 +0200

    adapt nested_dissection with new separator

--- src/dissection.jl
+++ src/dissection.jl
@@ -1,22 +1,22 @@
 """
-    separator(root_graph, subgraph_nodes)
-Call flowcutter (with s and t random) and pick nodes at random in the cut matching the 60% imbalance
-heuristic to form a separator. Then split the graph with respect to the separator.
+    separator!(g, subgraph_nodes)
+Call flowcutter (with s and t random) on g and pick nodes at random in the cut matching the 60% imbalance
+heuristic to form a separator. Then split the graph g with respect to the separator.
 
 # Arguments
-- `g::SimpleGraph{Int64}`: the graph to run flowcutter.
+- `g::SimpleGraph{Int64}`: the subgraph to run flowcutter.
+- `subgraph_nodes::Vector{Int64}`: a table where element at index i is corresponding label in the original root graph.
 
 # Return
 - `sep::Vector{Int64}`: the set of indices of the nodes in separator.
-- `split_parts::Array{Array{Int64,1},1}` several parts of the graph resulted from  separation.
+- `split_parts::Array{Array{Int64,1},1}` several parts of the graph resulted from separation.
 """
-function separator(
-    root_graph::SimpleGraph{Int64},
+function separator!(
+    g::SimpleGraph{Int64},
     subgraph_nodes::Vector{Int64},
 )::Tuple{Vector{Int64},Array{Array{Int64,1},1}}
 
     s, t = sample(subgraph_nodes, 2, replace = false)
-    g = induced_subgraph(root_graph, subgraph_nodes)[1]
     dist = floyd_warshall_shortest_paths(g).dists
     # 60% imbalance is like index = 40% of length(cuts) (as last cuts are close to 0% imbalance)
     # TODO: improve this, probably not what we want
@@ -37,7 +37,7 @@ function separator(
     end
     # catch the multiple parts created from the split
     split_parts = connected_components(g)
-    # be careful to return indices which make sense in the original root_graph
+    # be careful to return indices which make sense in the original root graph
     return (
         map(node -> subgraph_nodes[node], sep),
         map(
@@ -66,34 +66,37 @@ function nested_dissection(g::SimpleGraph{Int64})
     n = nv(g)
     order = zeros(Int64, n)
     treewidth = 0
-    q = Queue{Pair{SimpleGraph{Int64},Vector{Int64}}}()
-    enqueue!(q, g => Vector(1:n))
+    q = Queue{Vector{Int64}}()
+    enqueue!(q, Vector(1:n))
     i = n
     while !isempty(q)
-        graph, indices = dequeue!(q)
+        subgraph_nodes = dequeue!(q)
+        graph = induced_subgraph(g, subgraph_nodes)[1]
         # if graph is a tree or complete we can stop
         n = nv(graph)
         nedges = ne(graph)
         if nedges == n * (n - 1) / 2
             treewidth = max(treewidth, n - 1)
-            order[(i-n+1):i] = indices[1:n]
+            order[(i-n+1):i] .= subgraph_nodes
             i -= n
             continue
         elseif nedges == n - 1
             treewidth = max(treewidth, 1)
             # TODO: change this order to a bottom to top order for tree real tw
-            order[(i-n+1):i] = indices[1:n]
+            order[(i-n+1):i] .= subgraph_nodes
             i -= n
             continue
         end
+
         # compute separator and cut graph in several parts (graph and indices)
-        sep = separator(graph, indices)
-        toqueue = separate(graph, sep)
+        sep, toqueue = separator!(graph, subgraph_nodes)
+
         # update order and treewidth
         k = length(sep)
         order[(i-k+1):i] = sep
         i -= k
         treewidth = max(treewidth, k)
+
         # add next subgraphs to the queue
         enqueue!.([q], toqueue)
     end
