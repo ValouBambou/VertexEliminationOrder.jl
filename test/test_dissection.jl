119b0f97c6bbc46f8eaf9bb38414b4ff043299b1
Valentin Trophime <theval358@gmail.com>
2021-08-03 09:15:05 +0200

    better test

--- test/test_dissection.jl
+++ test/test_dissection.jl
@@ -15,7 +15,26 @@ nodes = [j for j in 1:9]
 @test tree_order!(G, nodes) == [9, 8, 7, 6, 4, 5, 3, 2, 1]
 
 
-G = smallgraph("house")
+for i in 1:12 add_vertex!(G) end
+# 1 to 5 is a clique
+for i in 1:5
+    for j in (i+1):5
+        add_edge!(G, i, j)
+    end
+end
+# 6 and 7 are midlle nodes
+add_edge!(G, 5, 6)
+add_edge!(G, 6, 7)
+add_edge!(G, 6, 8)
+# 8 to 12 is a tree
+add_edge!(G, 8, 9)
+add_edge!(G, 8, 10)
+add_edge!(G, 9, 11)
+add_edge!(G, 9, 12)
 
-@info "Testing nested_dissection with house graph"
-@info nested_dissection(G)
+@info "Testing nested_dissection with custom graph"
+res = nested_dissection(G)
+@info res
+# 6 should be separator
+@test res[1][12] == 6
+@test res[2] == 4
