975eac634c5d9254e8b06bdb535765389bab663e
Valentin Trophime <theval358@gmail.com>
2021-08-10 11:09:51 +0200

    add a test to run

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 
 @testset "VertexEliminationOrder.jl" begin
-
+    include("test_utils.jl")
     include("test_heuristics.jl")
     include("test_flowcutter.jl")
     include("test_dissection.jl")
