9d89f680904d7c2329d1573bf71654bffccb48c7
Valentin Trophime <theval358@gmail.com>
2021-08-17 16:01:56 +0200

    include wrapper

--- src/VertexEliminationOrder.jl
+++ src/VertexEliminationOrder.jl
@@ -5,6 +5,7 @@ using DataStructures
 using StatsBase
 using Parameters
 using SparseArrays
+using Random
 
 include("utils.jl")
 export treewidth_by_elimination!, tree_order!, graph_from_gr, square_lattice_graph
@@ -14,6 +15,8 @@ include("flowcutter.jl")
 export augment_flow!, forward_grow!, piercing_node, flowcutter!
 include("dissection.jl")
 export iterative_dissection
+include("mainwrapper.jl")
+export sample_iterative_dissections
 
 
 end
