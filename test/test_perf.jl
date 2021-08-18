0063eda85e4819a28cafd01a3c0f845c99ad1042
Valentin Trophime <theval358@gmail.com>
2021-08-18 13:36:50 +0200

    remove useless dependancies

--- test/test_perf.jl
+++ test/test_perf.jl
@@ -1,7 +1,9 @@
 using LightGraphs
-using VertexEliminationOrder
 using Test
-using ProfileView
+using Pkg
+
+Pkg.activate(".")
+using VertexEliminationOrder
 
 
 # The file to read the graph from.
@@ -10,7 +12,6 @@ graph_file = "test/example_graphs/sycamore_53_20.gr"
 ENV["JULIA_DEBUG"]=VertexEliminationOrder
 g = smallgraph("house")
 iterative_dissection(g)
-g = graph_from_gr(graph_file)
 
-@info "running wrapper function for 30s"
-ProfileView.@profview sample_iterative_dissections(g, 30)
+g = graph_from_gr(graph_file)
+@time iterative_dissection(g)
